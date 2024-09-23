3ee5adcc88e8786177c56f3012481fa47fc42bd0
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-09-23 15:58:11 +0530

    centrollers and routes and updation in models

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,11 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/drive', driveRoutes);
+app.use('/api/profile', profileRoutes);
+
 app.get("/" , (req , res) => {
     // return res.send("<p>Hello World!</p>") 
     return res.json({
