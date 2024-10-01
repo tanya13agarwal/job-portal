5093b2c5b72dabdb6fc87ebd1efa2d07df0afc79
Tanya Agarwal <135453589+tanya13agarwal@users.noreply.github.com>
2024-10-01 13:44:13 +0530

    Updated login/signup buttons

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import  { Link }  from "react-router-dom";
-import logo from "../../assets/images/logo.png"
+import logo from "../../assets/images/logo.jpeg"
 
 const Navbar = () => {
+
+  const handleLogin = () => {
+
+  }
+  const handleSignup = () => {
+
+  }
+
   return (
-    <div className='h-14 flex items-center justify-between bg-white rounded shadow-xl'>
+    <div className='flex items-center justify-between bg-white rounded shadow-xl'>
       <div>
-        <img src = {logo} alt='Logo' className='w-[10%] h-[12%]'/>
+        <img src = {logo} alt='Logo' className='m-2 w-[150px] h-[70px]'/>
       </div>
       <div className='flex items-center gap-4'>
         <Link to={""}>
@@ -28,10 +36,9 @@ const Navbar = () => {
            Statistics
         </Link>
       </div>
-      <div>
-        <button className=''>
-          Login / Signup
-        </button>
+      <div className='flex gap-3 items-center mr-4'>
+        <button onClick={handleLogin} className='px-4 py-2 text-white rounded active:scale-90 bg-customDarkBlue transition-all duration-200 hover:bg-white hover:text-customDarkBlue hover:border hover:border-customDarkBlue'>Login</button>
+        <button onClick={handleSignup} className='px-4 py-2 text-white rounded active:scale-90 bg-customDarkBlue transition-all duration-200 hover:bg-white hover:text-customDarkBlue hover:border hover:border-customDarkBlue'>Signup</button>
       </div>
     </div>
   )
