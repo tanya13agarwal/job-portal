fe0be0278c30829a377da927245ff746daa1605c
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-10-06 21:58:17 +0530

    updated the contactand placement sections

--- frontend/src/components/core/Contact.jsx
+++ frontend/src/components/core/Contact.jsx
@@ -21,63 +21,75 @@ const Contact = () => {
 
   return (
     <div id='contact' className="p-10">
-      <div className="bg-gradient-to-t bg-white h-screen flex items-center justify-evenly relative">
-        <div className="relative w-full max-w-sm p-6 bg-[#EAE4DD] rounded-lg shadow-lg mr-8 z-10">
-          <h1 className="text-2xl font-bold text-gray-800 mb-4">Contact Us</h1>
+      <div className="flex items-center w-full justify-between relative">
+        <div className="relative  p-6 w-[40%] mr-8 z-10">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 flex items-center justify-center">Contact Us</h1>
 
-          <form onSubmit={handleSubmit}>
-            <label className="block mb-2 text-gray-600">Name</label>
-            <input 
-              type="text" 
-              name="name" 
-              value={formData.name} 
-              onChange={handleChange} 
-              className="w-full mb-3 p-2 border border-gray-300 rounded" 
-              placeholder="abcd" 
-              required 
-            />
+          <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
+            <label className='mb-2'>
+              <p className='mb-1 text-[0.875rem] leading-[1.375rem] '>Full Name</p>
+              <input 
+                type="text" 
+                name="name" 
+                value={formData.name} 
+                onChange={handleChange} 
+                style={{boxShadow:"inset 0px -1px 0px rgba(255, 255, 255, 0.18)",}}
+                className='w-full rounded-[0.5rem] p-[12px] border-none' 
+                placeholder="abcd" 
+                required 
+              />
+            </label>
 
-            <label className="block mb-2 text-gray-600">Email</label>
-            <input 
-              type="email" 
-              name="email" 
-              value={formData.email} 
-              onChange={handleChange} 
-              className="w-full mb-3 p-2 border border-gray-300 rounded" 
-              placeholder="abcd@efg.com" 
-              required 
-            />
+            <label className='mb-2'>
+              <p className='mb-1 text-[0.875rem] leading-[1.375rem] '>Email Address</p>
+              <input 
+                type="email" 
+                name="email" 
+                value={formData.email} 
+                onChange={handleChange} 
+                style={{boxShadow:"inset 0px -1px 0px rgba(255, 255, 255, 0.18)",}}
+                className='w-full rounded-[0.5rem] p-[12px] border-none' 
+                placeholder="abcd@efg.com" 
+                required 
+              />
+            </label>
 
-            <label className="block mb-2 text-gray-600">Subject</label>
-            <input 
-              type="text" 
-              name="subject" 
-              value={formData.subject} 
-              onChange={handleChange} 
-              className="w-full mb-3 p-2 border border-gray-300 rounded" 
-              placeholder="Placement enquiry" 
-              required 
-            />
+            <label className='mb-2'>
+              <p className='mb-1 text-[0.875rem] leading-[1.375rem] '>Subject</p>
+              <input 
+                type="text" 
+                name="subject" 
+                value={formData.subject} 
+                onChange={handleChange} 
+                style={{boxShadow:"inset 0px -1px 0px rgba(255, 255, 255, 0.18)",}}
+                className='w-full rounded-[0.5rem] p-[12px] border-none' 
+                placeholder="Placement enquiry" 
+                required 
+              />
+            </label>
 
-            <label className="block mb-2 text-gray-600">Message</label>
-            <textarea 
-              name="message" 
-              value={formData.message} 
-              onChange={handleChange} 
-              className="w-full mb-3 p-2 border border-gray-300 rounded" 
-              placeholder="Enter your message..." 
-              required 
-            ></textarea>
+            <label className='mb-2'>
+              <p className='mb-1 text-[0.875rem] leading-[1.375rem] '>Message</p>
+              <textarea 
+                name="message" 
+                value={formData.message} 
+                onChange={handleChange} 
+                style={{boxShadow:"inset 0px -1px 0px rgba(255, 255, 255, 0.18)",}}
+                className='w-full rounded-[0.5rem] p-[12px] border-none' 
+                placeholder="Enter your message..." 
+                required 
+              ></textarea>
+            </label>
 
-            <button type="submit" className="w-full py-2 px-4 bg-customDarkBlue text-white rounded hover:scale-105 transition-all duration-200">
+            <button type="submit" className="w-full py-2 px-4 bg-customDarkBlue text-white rounded hover:scale-95 transition-all duration-200">
               Send Message
             </button>
           </form>
         </div>
 
 
-        <div className="w-1/3 bg-[#EAE4DD] flex flex-col gap-1 justify-center  p-10 ">
-          <h2 className="text-2xl font-bold mb-4">Contact Information :-</h2>
+        <div className="bg-[#EAE4DD] w-[50%] flex flex-col gap-1 justify-center  p-10 ">
+          <h2 className="text-2xl font-bold mb-4 flex items-center justify-center">Contact Information</h2>
           <div className='flex flex-col gap-1'>
             <div className='flex items-start gap-2 '>
               <p className='text-lg font-semibold '>Location:</p>
