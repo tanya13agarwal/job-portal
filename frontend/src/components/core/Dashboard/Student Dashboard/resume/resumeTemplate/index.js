d1402a0034021e687ac9f4f90db83c8ca6abf7e8
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-11-01 10:12:11 +0530

    created resume template skeleton

--- frontend/src/components/core/Dashboard/Student Dashboard/resume/resumeTemplate/index.js
+++ frontend/src/components/core/Dashboard/Student Dashboard/resume/resumeTemplate/index.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import { IoMdArrowRoundBack } from "react-icons/io";
+
 
 const ResumeTemplate = () => {
   return (
-    <div>ResumeTemplate</div>
+    <div className = 'grid sm:grid-cols-2 grid-cols-1 col-span-1 gap-10 justify-between'>
+      <div class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Software Developer</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+        <a href="" class="px-4 flex items-center gap-2 w-fit py-2 rounded border border-transparent active:scale-90 text-[#fff] bg-customDarkBlue transition-all duration-200 hover:bg-transparent hover:text-black hover:border-[0.5px] hover:border-customDarkBlue">
+            <p className='text-nowrap'>Create</p>
+            <IoMdArrowRoundBack className='rotate-180'/>
+        </a>
+      </div>
+      <div class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Data Science</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+        <a href="" class="px-4 flex items-center gap-2 w-fit py-2 rounded border border-transparent active:scale-90 text-[#fff] bg-customDarkBlue transition-all duration-200 hover:bg-transparent hover:text-black hover:border-[0.5px] hover:border-customDarkBlue">
+            <p className='text-nowrap'>Create</p>
+            <IoMdArrowRoundBack className='rotate-180'/>
+        </a>
+      </div>
+      <div class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Full Stack Developer</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+        <a href="" class="px-4 flex items-center gap-2 w-fit py-2 rounded border border-transparent active:scale-90 text-[#fff] bg-customDarkBlue transition-all duration-200 hover:bg-transparent hover:text-black hover:border-[0.5px] hover:border-customDarkBlue">
+            <p className='text-nowrap'>Create</p>
+            <IoMdArrowRoundBack className='rotate-180'/>
+        </a>
+      </div>
+      <div class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Create Your Own</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+        <a href="" class="px-4 flex items-center gap-2 w-fit py-2 rounded border border-transparent active:scale-90 text-[#fff] bg-customDarkBlue transition-all duration-200 hover:bg-transparent hover:text-black hover:border-[0.5px] hover:border-customDarkBlue">
+            <p className='text-nowrap'>Create</p>
+            <IoMdArrowRoundBack className='rotate-180'/>
+        </a>
+      </div>
+    </div>
   )
 }
 
