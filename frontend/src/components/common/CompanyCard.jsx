a1fa45fae5a0b51fafcc6eb4e41ab93be3f29ec0
Chandni Kalwani <kalwanichandni24@gmail.com>
2024-11-10 11:20:39 +0530

    Update CompanyCard.jsx

--- frontend/src/components/common/CompanyCard.jsx
+++ frontend/src/components/common/CompanyCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import DescriptionModal from './DescriptionModal';
 import useOnClickOutside from '../../hooks/useOnClickOutside';
 import { useNavigate } from 'react-router-dom';
@@ -8,13 +8,21 @@ const CompanyCard = ({
     image ,
     CompanyName , 
     Position , 
-    description , 
-    jobDesc , 
+    JobDescription , 
+    jobDescPDF , 
     status = "Draft" , 
     compbtn1 = "Apply Now" , 
     compbtn2 = "Take Test",
     btn1Handler,
-    btn2Handler
+    btn2Handler,
+    companyWebsite,
+    Batch,
+    Branch,
+    Stipend,
+    MinSalary,
+    MaxSalary,
+    JobLocation,
+    Requirements
 }) => {
     console.log(status)
     const [disable , setDisable] = useState(true);
@@ -41,17 +49,26 @@ const CompanyCard = ({
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{CompanyName}</h5>
             <span className="text-sm text-gray-500 dark:text-gray-400">{Position}</span>
             <div onClick={(e) => e.stopPropagation()} className="flex items-center gap-3 mt-4 md:mt-6">
-                <button onClick={btn1Handler} className="px-4 py-2 rounded border border-transparent active:scale-90 text-[#fff] bg-customDarkBlue transition-all duration-200 hover:bg-transparent hover:text-black hover:border-[0.5px] hover:border-customDarkBlue ">{compbtn1}</button>
-                <button onClick={btn2Handler} disabled = {disable} href="/test" className={`${disable ? "text-gray-400 bg-gray-200" : "active:scale-90 transition-all duration-200 border border-customDarkBlue hover:bg-customDarkBlue hover:text-[#fff]"} px-4 py-2 rounded `}>{compbtn2}</button>
+                <button onClick={btn1Handler} className="text-nowrap px-4 py-2 rounded border border-transparent active:scale-90 text-[#fff] bg-customDarkBlue transition-all duration-200 hover:bg-transparent hover:text-black hover:border-[0.5px] hover:border-customDarkBlue ">{compbtn1}</button>
+                <button onClick={btn2Handler} disabled = {disable} href="/test" className={`${disable ? "text-gray-400 bg-gray-200" : "active:scale-90 transition-all duration-200 border border-customDarkBlue hover:bg-customDarkBlue hover:text-[#fff]"} px-4 py-2 rounded text-nowrap`}>{compbtn2}</button>
             </div>
         </div>
         {
             descModal && <DescriptionModal 
-                            text1 = {`${Position} , ${CompanyName}`}
-                            text2 = {description}
+                            compName ={CompanyName}
+                            jobRole={Position}
+                            batch={Batch}
+                            branch={Branch}
+                            stipend={Stipend}
+                            JobDescription={JobDescription}
+                            MinSalary={MinSalary}
+                            MaxSalary={MaxSalary}
+                            location={JobLocation}
+                            companyWebsite={companyWebsite}
+                            requirements={Requirements}
                             btn1Text = "Job Description"
                             btn2Text = "Close"
-                            btn1Handler = {jobDesc}
+                            btn1Handler = {jobDescPDF}
                             btn2Handler = {toggleHandler}
                         />
         }
