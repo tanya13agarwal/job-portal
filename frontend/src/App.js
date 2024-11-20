91d3a3c0743a58a4ddddcfda936555c7f6ab911d
kalwanichandni24 <kalwanichandni24@gmail.com>
2024-11-20 14:55:45 +0530

    added lazy in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,154 +1,103 @@
-import React from "react";
-import {Routes , Route} from "react-router-dom";
-// import Navbar from "./components/common/Navbar";
+import React, { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 import { ACCOUNT_TYPE } from "./utils/accoutnType";
 import { useSelector } from "react-redux";
-// import About from "./components/core/About";
 
-
-// import { Features } from "./components/core/Features";
-// import Placements from "./components/core/Placements";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import VerifyEmail from "./pages/VerifyEmail";
-import Test from "./components/core/Dashboard/Student Dashboard/OnCampus/Test";
-import OnCampus from "./components/core/Dashboard/Student Dashboard/OnCampus";
-import MockTest from "./components/core/Dashboard/Student Dashboard/MockTest/MockTest";
-import AdminLogin from "./pages/Admin/AdminLogin";
-import PrivateRoute from "./components/core/Auth/PrivateRoute";
-import Dashboard from "./pages/Dashboard";
-import MyProfile from "./components/core/Dashboard/MyProfile";
-import Settings from "./components/core/Dashboard/Settings";
-import { ApplyJob } from "./components/core/Dashboard/Student Dashboard/ApplyJob";
-import Resume from "./components/core/Dashboard/Student Dashboard/resume";
-import PlacementDashboard from "./components/core/Dashboard/Placement Dashboard/Dashboard"
-import StudentDashboard from "./components/core/Dashboard/Student Dashboard/StudentDashboard";
-import StudentData from "./components/core/Dashboard/Placement Dashboard/studentData";
-import Table from "./components/core/Dashboard/Placement Dashboard/studentData/Table";
-import Companies from "./components/core/Companies";
-import JobView from "./components/core/Dashboard/Placement Dashboard/JobView";
-import JobPosting from "./components/core/Dashboard/Placement Dashboard/jobPosting";
-import PlacementRecords from "./components/core/PlacementRecords";
-import CreateResume from "./components/core/Dashboard/Student Dashboard/resume/features/CreateResume";
-import ChooseTemplate from "./components/core/Dashboard/Student Dashboard/resume/features/ChooseTemplate";
-import ResumeSamples from "./components/core/Dashboard/Student Dashboard/resume/features/ResumeSamples";
-import ATSScoreChecker from "./components/core/Dashboard/Student Dashboard/resume/features/ATSScoreChecker";
-
-import AdminDashboard from "./components/core/Dashboard/Admin Dashboard/AdminDashboard";
-import Error from "./pages/Error";
-import Courses from "./components/core/Dashboard/Student Dashboard/Courses";
-
-
+// Lazy loading the components
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Signup = lazy(() => import("./pages/Signup"));
+const VerifyEmail = lazy(() => import("./pages/VerifyEmail"));
+const Test = lazy(() => import("./components/core/Dashboard/Student Dashboard/OnCampus/Test"));
+const OnCampus = lazy(() => import("./components/core/Dashboard/Student Dashboard/OnCampus"));
+const MockTest = lazy(() => import("./components/core/Dashboard/Student Dashboard/MockTest/MockTest"));
+const AdminLogin = lazy(() => import("./pages/Admin/AdminLogin"));
+const PrivateRoute = lazy(() => import("./components/core/Auth/PrivateRoute"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const MyProfile = lazy(() => import("./components/core/Dashboard/MyProfile"));
+const Settings = lazy(() => import("./components/core/Dashboard/Settings"));
+const ApplyJob = lazy(() => import("./components/core/Dashboard/Student Dashboard/ApplyJob"));
+const Resume = lazy(() => import("./components/core/Dashboard/Student Dashboard/resume"));
+const PlacementDashboard = lazy(() => import("./components/core/Dashboard/Placement Dashboard/Dashboard"));
+const StudentDashboard = lazy(() => import("./components/core/Dashboard/Student Dashboard/StudentDashboard"));
+const StudentData = lazy(() => import("./components/core/Dashboard/Placement Dashboard/studentData"));
+const Table = lazy(() => import("./components/core/Dashboard/Placement Dashboard/studentData/Table"));
+const Companies = lazy(() => import("./components/core/Companies"));
+const JobView = lazy(() => import("./components/core/Dashboard/Placement Dashboard/JobView"));
+const JobPosting = lazy(() => import("./components/core/Dashboard/Placement Dashboard/jobPosting"));
+const PlacementRecords = lazy(() => import("./components/core/PlacementRecords"));
+const CreateResume = lazy(() => import("./components/core/Dashboard/Student Dashboard/resume/features/CreateResume"));
+const ChooseTemplate = lazy(() => import("./components/core/Dashboard/Student Dashboard/resume/features/ChooseTemplate"));
+const ResumeSamples = lazy(() => import("./components/core/Dashboard/Student Dashboard/resume/features/ResumeSamples"));
+const ATSScoreChecker = lazy(() => import("./components/core/Dashboard/Student Dashboard/resume/features/ATSScoreChecker"));
+const AdminDashboard = lazy(() => import("./components/core/Dashboard/Admin Dashboard/AdminDashboard"));
+const Error = lazy(() => import("./pages/Error"));
+const Courses = lazy(() => import("./components/core/Dashboard/Student Dashboard/Courses"));
 
 function App() {
-
-  const {user} = useSelector((state) => state.profile);
+  const { user } = useSelector((state) => state.profile);
 
   return (
     <div className="bg-customBeige w-screen min-h-screen flex flex-col font-inter">
-      {/* <Navbar/> */}
-      <Routes>
-        
-        <Route path="/" element = {<Home/>}/> 
-        <Route path="/companies" element = {<Companies/>}/> 
-        <Route path="/placement-records" element = {<PlacementRecords/>}/> 
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/placement-records" element={<PlacementRecords />} />
+          <Route path="login" element={<Login />} />
+          <Route path="admin" element={<AdminLogin />} />
+          <Route path="placement" element={<AdminLogin />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="verify-email" element={<VerifyEmail />} />
 
+          <Route
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          >
+            <Route path="dashboard/my-profile" element={<MyProfile />} />
+            <Route path="dashboard/Settings" element={<Settings />} />
 
-        {/* <Route path="catalog/:catalogName" element = {<Catalog/>}/> */}
-        
-        <Route path="login" element = {<Login/>}/>
-
-        <Route path="admin" element = {<AdminLogin/>}/>
-        
-        <Route path="placement" element = {<AdminLogin/>}/>
-        
-        <Route path="signup" element = {<Signup/>}/>
-        
-        <Route path="verify-email" element = {<VerifyEmail/>}/>
-        
-        
-        
-        {/* <Switch> */}
-          {/* <Route path="/test" component={Test} /> */}
-        {/* </Switch> */}
-
-       
-        
-        {/* <Route path="forgot-password" element = {<ForgotPassword/>}/> */}
-        
-        
-        {/* <Route path="about" element = {<About/>}/>
-        <Route path="features" element = {<Features/>}/>
-        <Route path="placements" element = {<Placements/>}/> */}
-        
-        {/* /*<Route path="update-password/:id" element = {<UpdatePassword/>}/>  */}
-        
-        
-        
-        <Route element = {
-          <PrivateRoute>
-            <Dashboard/>
-          </PrivateRoute>
-        }>
-
-          <Route path="dashboard/my-profile" element = {<MyProfile/>}/>
-          <Route path="dashboard/Settings" element={<Settings />} />
-
-          {
-            user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            {user?.accountType === ACCOUNT_TYPE.STUDENT && (
               <>
-                
-                <Route path="/dashboard/mock-test" element={<MockTest/>} />
-                <Route path="/dashboard/on-campus" element = {<OnCampus/>}/>
-                <Route path="/dashboard/on-campus/test" element = {<Test/>}/>
-                <Route path="/dashboard/on-campus/apply/:jobId" element = {<ApplyJob/>}/>
-                <Route path="/dashboard/resume" element = {<Resume/>}/>
-                <Route path="/dashboard/courses" element = {<Courses/>}/>
-                <Route path="/dashboard/student" element = {<StudentDashboard/>}/>
-                <Route path="/dashboard/resume/createResume" element = {<CreateResume/>}/>
-                <Route path="/dashboard/resume/chooseTemplate" element = {<ChooseTemplate/>}/>
-                <Route path="/dashboard/resume/resumeSamples" element = {<ResumeSamples/>}/>
-                <Route path="/dashboard/resume/atsScoreChecker" element = {<ATSScoreChecker/>}/>
+                <Route path="/dashboard/mock-test" element={<MockTest />} />
+                <Route path="/dashboard/on-campus" element={<OnCampus />} />
+                <Route path="/dashboard/on-campus/test" element={<Test />} />
+                <Route path="/dashboard/on-campus/apply/:jobId" element={<ApplyJob />} />
+                <Route path="/dashboard/resume" element={<Resume />} />
+                <Route path="/dashboard/courses" element={<Courses />} />
+                <Route path="/dashboard/student" element={<StudentDashboard />} />
+                <Route path="/dashboard/resume/createResume" element={<CreateResume />} />
+                <Route path="/dashboard/resume/chooseTemplate" element={<ChooseTemplate />} />
+                <Route path="/dashboard/resume/resumeSamples" element={<ResumeSamples />} />
+                <Route path="/dashboard/resume/atsScoreChecker" element={<ATSScoreChecker />} />
               </>
-            )
-          }
-          
-          
-          {
-            user?.accountType === ACCOUNT_TYPE.PLACEMENT_CELL && (
-              <>
-                <Route path="/dashboard/job-posting" element={<JobPosting/>} />
-                <Route path="/dashboard/jobs" element={<JobView/>} />
-                <Route path="/dashboard/placement" element={<PlacementDashboard/>} />
-                <Route path="/dashboard/student-data" element={<StudentData/>} />
-                <Route path="/dashboard/student-data/all-students" element={<Table/>} />
-                {/* <Route path="dashboard/add-course" element = {<AddCourse/>}/>
-                <Route path="dashboard/my-courses" element = {<MyCourses/>}/>
-                <Route path="dashboard/edit-course/:courseId" element = {<EditCourse/>}/>      */}
-              </>
-            )
-          }
-          
-          
-          {
-            user?.accountType === ACCOUNT_TYPE.ADMIN && (
-              <>
-                <Route path="/dashboard/student-data" element={<StudentData/>} />
-                <Route path="/dashboard/student-data/all-students" element={<Table/>} />
-                <Route path="/dashboard/admin" element={<AdminDashboard/>} />
-                {/* <Route path="dashboard/add-course" element = {<AddCourse/>}/>
-                <Route path="dashboard/my-courses" element = {<MyCourses/>}/>
-                <Route path="dashboard/edit-course/:courseId" element = {<EditCourse/>}/>      */}
-              </>
-            )
-          }
-        
-        </Route>
+            )}
 
-        <Route path="*" element={<Error/>}/>
-      
-      </Routes>
+            {user?.accountType === ACCOUNT_TYPE.PLACEMENT_CELL && (
+              <>
+                <Route path="/dashboard/job-posting" element={<JobPosting />} />
+                <Route path="/dashboard/jobs" element={<JobView />} />
+                <Route path="/dashboard/placement" element={<PlacementDashboard />} />
+                <Route path="/dashboard/student-data" element={<StudentData />} />
+                <Route path="/dashboard/student-data/all-students" element={<Table />} />
+              </>
+            )}
+
+            {user?.accountType === ACCOUNT_TYPE.ADMIN && (
+              <>
+                <Route path="/dashboard/student-data" element={<StudentData />} />
+                <Route path="/dashboard/student-data/all-students" element={<Table />} />
+                <Route path="/dashboard/admin" element={<AdminDashboard />} />
+              </>
+            )}
+          </Route>
+
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
