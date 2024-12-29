8982641768e843549111a638c573c5a12d97f70c
SaumyaSachan <saumya2111054@akgec.ac.in>
2024-12-30 00:27:59 +0530

    changes in deleteAccount

--- frontend/src/components/core/Dashboard/Settings/DeleteAccount.jsx
+++ frontend/src/components/core/Dashboard/Settings/DeleteAccount.jsx
@@ -1,3 +1,50 @@
+// import { FiTrash2 } from "react-icons/fi"
+// import { useDispatch, useSelector } from "react-redux"
+// import { useNavigate } from "react-router-dom"
+
+// import { deleteProfile } from "../../../../services/operations/SettingsAPI"
+
+// export default function DeleteAccount() {
+//   const { token } = useSelector((state) => state.auth)
+//   const dispatch = useDispatch()
+//   const navigate = useNavigate()
+
+//   async function handleDeleteAccount() {
+//     try {
+//       dispatch(deleteProfile(token, navigate))
+//     } catch (error) {
+//       console.log("ERROR MESSAGE - ", error.message)
+//     }
+//   }
+
+//   return (
+//     <>
+//       <div className="my-10 flex flex-row gap-x-5 rounded-md border-[1px] border-pink-700 bg-pink-900 p-8 px-12">
+//         <div className="flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-pink-700">
+//           <FiTrash2 className="text-3xl text-pink-200" />
+//         </div>
+//         <div className="flex flex-col space-y-2">
+//           <h2 className="text-lg font-semibold text-richblack-5">
+//             Delete Account
+//           </h2>
+//           <div className="w-4/5 text-pink-25">
+//             <p>Would you like to delete account?</p>
+//             <p>
+//               Deleting your account is permanent and will remove all the contain associated with it.
+//             </p>
+//           </div>
+//           <button
+//             type="button"
+//             className="w-fit cursor-pointer italic text-pink-300"
+//             onClick={handleDeleteAccount}
+//           >
+//             I want to delete my account.
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
 import { FiTrash2 } from "react-icons/fi"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
@@ -18,30 +65,27 @@ export default function DeleteAccount() {
   }
 
   return (
-    <>
-      <div className="my-10 flex flex-row gap-x-5 rounded-md border-[1px] border-pink-700 bg-pink-900 p-8 px-12">
-        <div className="flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-pink-700">
-          <FiTrash2 className="text-3xl text-pink-200" />
-        </div>
-        <div className="flex flex-col space-y-2">
-          <h2 className="text-lg font-semibold text-richblack-5">
-            Delete Account
-          </h2>
-          <div className="w-4/5 text-pink-25">
-            <p>Would you like to delete account?</p>
-            <p>
-              Deleting your account is permanent and will remove all the contain associated with it.
-            </p>
-          </div>
-          <button
-            type="button"
-            className="w-fit cursor-pointer italic text-pink-300"
-            onClick={handleDeleteAccount}
-          >
-            I want to delete my account.
-          </button>
-        </div>
+    <div className="my-10 flex flex-col gap-y-5 rounded-md border-[1px] border-pink-700 bg-pink-900 p-5 sm:flex-row sm:gap-x-5 sm:p-8 sm:px-12">
+      <div className="flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-pink-700 mx-auto sm:mx-0">
+        <FiTrash2 className="text-3xl text-pink-200" />
       </div>
-    </>
+      <div className="flex flex-col items-center text-justify  space-y-3 sm:items-start sm:text-left">
+        <h2 className="text-lg font-semibold text-richblack-5">
+          Delete Account
+        </h2>
+        <div className="w-full text-pink-25 sm:w-4/5">
+          <p>Would you like to delete account?</p>
+          <p>
+            Deleting your account is permanent and will remove all the content associated with it.
+          </p>
+        </div>
+        <button
+          type="button"
+          className="w-fit cursor-pointer italic text-pink-300 ml-0"
+          onClick={handleDeleteAccount}>
+          I want to delete my account.
+        </button>
+      </div>
+    </div>
   )
 }
